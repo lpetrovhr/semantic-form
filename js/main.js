065899638e219661d9e97e55b8b5a3d5c8450b98
leon <lpetrovhr@gmail.com>
2016-05-31 11:58:49 +0200

    main js change

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ if(msieversion() < 9) { document.getElementById('revealPassword').style.display 
 
 var checked = false;
 
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+
 $('#revealPassword').click(function() {
   changeCheckboxState();
 });
@@ -47,6 +49,41 @@ $('form').submit(function(e) {
     document.getElementById('cardNumber').focus();
   }
 });
+
+if(isSafari || msieversion() < 10) {
+  $('#paymentValidation').validate({
+    rules: {
+      webAddress: { required: true },
+      fullName: { required: true },
+      emailAdd: {
+        required: true,
+        email: true
+      },
+      passwrd: { required: true },
+      cardNumber: { required:true },
+      secCode: { required:true }
+      },
+      messages: {
+      webAddress: '<img src="img/mark.png"/><i class="errorMessage">Enter website name.</i>',
+      fullName: '<img src="img/mark.png"/><i class="errorMessage">Enter your full name.</i>',
+      emailAdd: '<img src="img/mark.png"/><i class="errorMessage">Enter correct email.</i>',
+      passwrd: '<img src="img/mark.png"/><i class="errorMessage">Password is required.</i>',
+      cardNumber: '<img src="img/mark.png"/><i class="errorMessage">Enter credit card number.</i>',
+      secCode: '<img src="img/mark.png"/><i class="errorMessage">Enter security code.</i>',
+      expirationYear: '<img src="img/mark.png"/><i class="errorMessage">Enter expiration year.</i>',
+      expMonth: '<img src="img/mark.png"/><i class="errorMessage">Enter expiration month.</i>'
+      },
+      errorPlacement: function (error, element) {
+      error.addClass('error--message_style');
+      if(element.attr('name') == 'expMonth') {
+          error.insertAfter('#expirationYear');
+      } else {
+          error.insertAfter(element);
+      }
+      },
+      errorElement: 'p'
+  });
+}
 
 function changeCheckboxState() {
   if(!checked) {
